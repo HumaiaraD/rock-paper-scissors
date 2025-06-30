a71a21176dda8f36da58d9f12aa016d7bfd81082
Humaiara Diea <d.humaiara@gmail.com>
2025-06-30 00:29:23 -0400

    Finished product

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const choices = ["rock", "paper", "scissors"];
 const playerDisplay = document.getElementById("pd");
 const computerDisplay = document.getElementById("cd");
 const result = document.getElementById("result");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+const resetBtn = document.getElementById('reset');
+let playerScore = 0;
+let computerScore =0;
 
 function playGame(playerChoice){
 
@@ -9,7 +14,8 @@ function playGame(playerChoice){
     let gameResult = "";
 
     if (playerChoice === computerChoice){
-        gameResult = "It's a TIE!"
+        gameResult = "It's a TIE!";
+        result.style.color = "rgb(66, 85, 228)";
     }
     else {
         switch(playerChoice){
@@ -29,4 +35,28 @@ function playGame(playerChoice){
     computerDisplay.textContent = `Computer: ${computerChoice}`;
     result.textContent = `Result: ${gameResult}`;
 
+    switch(gameResult){
+        case "You WIN!":
+            result.style.color = "green";
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
+            break;
+        case "You lose":
+            result.style.color = "red";
+            computerScore++;
+            computerScoreDisplay.textContent = computerScore;
+            break;
+    }
+
+}
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreDisplay.textContent = "0";
+    computerScoreDisplay.textContent = "0";
+    playerDisplay.textContent = "Player: ";
+    computerDisplay.textContent = "Computer: ";
+    result.textContent = "Result:";
+    result.style.color = "rgb(66, 85, 228)";
 }
